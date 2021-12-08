9f0ccb67f0d0cdbb6554d504ea8964f9f83108b6
amogus7337 <84619912+amogus7337@users.noreply.github.com>
2021-12-08 18:39:37 +0700

    Add files via upload

---
+++ проверка1.py
@@ -0,0 +1,10 @@
+login  = input ("введите login")
+num = input ("введите пароль")
+while(login != "Admin"or num != "445588"):
+    if login  ==  "Admin":
+      
+    print("login или  пароль не верный")
+    login  = input ("введите login")
+    num = input ("введите пароль")
+print("доброго дня ")
+    
